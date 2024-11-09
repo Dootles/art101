513b8c9ddba6298a0e1a0474db89f6c3bd861d81
Dootles <dooot985@gmail.com>
2024-11-08 16:00:29 -0800

    lab10 attempt

--- lab10/lab10.js
+++ lab10/lab10.js
@@ -5,18 +5,20 @@
    Author: Kiah Foskey
    Date: Nov 8th 2024
 */
-
 function generateRandomText() {
     const text = "Grandma got ran over by a raindeer! and I saw it happen.";
     const min = 3;
     const max = 100;
     const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
-
+    // Get a random starting index to slice the Lorem Ipsum text
     const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
-
+    // Generate the random Lorem Ipsum-like text
     return text.slice(randStart, randStart + randLen);
   }
-
-// click listener for button
-$("#talkie").click(function(){
+  // click listener for button
+$("#make-convo").click(function(){
 });
+// get new fake dialogue
+const newText = generateRandomText();
+// append a new div to our output div
+$("#output").append('<div class="text"><p>' + newText + '</p></div>');
