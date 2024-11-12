5869a40f0536a30d43b5b592c761da71a26c8fba
Dootles <dooot985@gmail.com>
2024-11-11 23:27:10 -0800

    lab 11

---
+++ lab11/lab11.js
@@ -0,0 +1,24 @@
+/*
+   lab.js
+
+   Requirements: jQuery must be loaded for this script to work.
+
+   Author: Kiah Foskey
+   Date: November 11, 2024
+*/
+
+
+function sortString(inputString) {
+    return inputString.split('').sort().join('');
+  }
+
+$("#submit").click(function(){
+});
+
+const user = $("#user").val();
+
+// now let's sort it
+userNameSorted = sortString(user);
+
+// append a new div to our output div
+$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
